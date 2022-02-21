09f61598e101f342ed964510e7c7a0c8ad686586
Tim Rager <tim.rager@gmail.com>
2022-02-21 10:41:41 -0800

    splash page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './style.css'
+
+// Two screens (start & questions)
+// Pull 5 questions from https://opentdb.com/api_config.php
+// Tally correct answers after "Checked answers" is clicked
+
 
 
 const App = () => {
   return (
-    <h1> TESTING </h1>
+    <div className="container">
+      <main>
+        <div className="blob-top"></div>
+        <h1> Quizzical </h1>
+        <h2> How Smart Are You? </h2>
+        <div className="button">Start Quiz!</div>
+        <div className="blob-btm"></div>
+      </main>
+    </div>
   )
 }
 
---
+++ src/Quiz.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css'
+
+
+const Quiz = () => {
+
+}
+
+
+export default Quiz;
